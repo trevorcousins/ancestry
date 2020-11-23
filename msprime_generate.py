a3e2c6145d4bbd0894bd26c471af7aae1fce04a5
Trevor Cousins <github@tcx.me.uk>
2020-11-23 17:41:21 +0000

    added msprime_generate and msprime_utils

---
+++ msprime_generate.py
@@ -0,0 +1,75 @@
+# script to simulate history, then save output coalescent data
+# example usage: python msprime_generate.py instant_struct0001 -L 30000000 -mig 0.1 -t 20000 40000 
+
+import os
+from datetime import datetime
+import argparse
+from msprime_models import * 
+# from abinitio_tm import scaled_time_intervals
+# from msprime_utils import scaled_time_intervals, get_het, round_coal_times, tm_counts, get_coal_data, round_bin_coal_data, normalise
+from msprime_utils import *
+from vcf_mhs import *
+import numpy as np
+import pdb
+import math
+import pandas as pd
+from scipy.stats import entropy
+
+# parse arguments
+#  instant_struct0001(N_0,mig_prop,t_1,t_2,seq_length,recomb_rate,mut_rate,print)
+parser = argparse.ArgumentParser()
+parser.add_argument("model",help="Specify the model to be used, as defined in msprime_models.")
+parser.add_argument("-N","--N_0",help="Initial population size (Default is 1e+04",default=1e+04,type=int)
+parser.add_argument("-L","--seq_length",help="Length of the sequence to be simulated (default 3e+07)",default=int(3e+07),type=int)
+parser.add_argument("-mig","--migration_prop",help="Proportion of migrating population (default 0.3)",default=0.3,type=float)
+parser.add_argument("-t","--time_splits",nargs=2,help="Time of splits, in generations (default t_1 = 2e+04, t_2 = 4e+04)",default = [int(2e+04),int(4e+04)])
+parser.add_argument("-bin","--bin_length",help="The length (in bases) of the how wide you want each bin. (Default is 100)",default=100,type=int)
+parser.add_argument("-o_coaldir","--output_coaldir",help="Output directory of coalescent data",default=os.getcwd() + '/coal_data/',type=str)
+parser.add_argument("-o_coalname","--output_coalname",help="Output name for coal_data filename",default='',type=str)
+parser.add_argument("-o_mhsdir","--output_mhsdir",help="Output dir for vcf and mhs data",default=os.getcwd() + '/vcf_mhs/',type=str)
+parser.add_argument("-o_mhsname","--output_mhsname",help="Output name for vcf and mhs data",default = '',type=str)
+parser.add_argument("-rho","--recomb_rate",help="Rate of recombination per bp per generation",default=2e-08,type=float)
+parser.add_argument("-mew","--mut_rate",help="Rate of mutation per bp per generation",default=2e-08,type=float)
+parser.add_argument("--print",help="Print the DemographicDebugger (from msprime) and the number of segratating sites",action="store_true")
+parser.add_argument("--tree",help="Draw trees and info per segment",action="store_true")
+args = parser.parse_args()
+
+# save args
+model = args.model
+print('msprime model is {}, as defined in msprime_models.py'.format(model))
+suffix = ""
+if args.output_coalname == '': # by default, save name as Ymd + model + migprop
+    coal_output = args.output_coaldir + datetime.now().strftime("%Y%m%d") + '_' + model + '_mig' + str(int(10*args.migration_prop))
+else:
+    coal_output = args.output_coaldir + datetime.now().strftime("%Y%m%d") + args.output_coalname 
+if args.output_mhsname == '':
+    mhs_output = args.output_mhsdir +  datetime.now().strftime("%Y%m%d") + '_' + model + '_mig' + str(int(10*args.migration_prop))
+else: 
+    mhs_output = args.output_mhsdir +  datetime.now().strftime("%Y%m%d") + args.output_mhsname
+
+
+if args.print:
+    print_ = True
+else: 
+    print_ = False
+
+# run simulation
+print('Running simulation of model {}'.format(model))
+command_line = 'sim = ' + model + '(' + str(args.N_0) + ',' + str(args.migration_prop) + ',' + str(args.time_splits[0]) + ',' + str(args.time_splits[1]) + ',' + str(args.seq_length) + ',' + str(args.recomb_rate) + ',' + str(args.mut_rate) + ',' + str(print_) + ')'
+exec('sim = ' + command_line)
+# str(print_) + ')' )
+# def instant_struct0001(N_0,mig_prop,t_1,t_2,seq_length,recomb_rate,mut_rate,print):
+# TODO ABOVE
+print('Simulation finished')
+  
+# save coalescent data to disc
+tmrca_data = np.array(get_coal_data(sim, args)) # get coalescent data, as np array
+header = "tree index, upper sequence interval,tMCRA"
+np.savetxt(coal_output + '.txt', tmrca_data, header=header, delimiter="\t") #write to disc
+
+# save vcf and mhs data to disc
+sim_to_mhs(sim,vcf_path = args.output_mhsdir,mhs_path = args.output_mhsdir, suffix = '_' + model + '_mig' + str(int(10*args.migration_prop)))
+
+# load this file
+# test = np.loadtxt(path/to/file,comments="#")
+
---
+++ msprime_utils.py
@@ -0,0 +1,98 @@
+# This script hosts different functions that are useful for msprime simulations 
+import math
+import numpy as np
+
+# generate time intervals as per Wang et. al. (2020), scaling them by the N_0 (long term effective pop size)
+def scaled_time_intervals(sim, N_T,mu = 2e-08, alpha = 0.1, Tmax = 15):
+    # parameters: sim is the msprime simulation; mu is the per bp per gen mutation rate. N_T is number of states
+    # ...alpha and Tmax are parameters defining the spacing of the intervals
+    
+    # heterozygosity is defined as the fraction of loci within an individual that are heterozygous.
+    # het = sim.get_pairwise_diversity() / sim.get_sequence_length()
+    het = get_het(sim)
+    # N_0 (long term effective population size) is defined as heterozygosity / 4mu
+    N_0 = het/(4*mu)
+    # define time boundaries
+    T = [0] # first lower boundary is 0 
+    for i in range(0,N_T): # TODO: should T have upper interval
+        T.append( alpha*math.exp( (i/N_T)*math.log(1 + Tmax/alpha) - 1))
+    T_np = np.array(T) # convert to numpy
+    # scale these to generations with *2*N_0, as per Schiffels' instruction
+    T_scaled_np = T_np * 2 * N_0
+    return T_scaled_np, T_np, N_0
+
+# function to return heterozygosity of an msprime simulation
+def get_het(sim):
+    het = sim.get_pairwise_diversity() / sim.get_sequence_length()
+    return het
+
+# function to round a sequence of coalescent times into their closest time interval
+def round_coal_times(coal_times,T_scaled_np,N_T):
+    # coal_times are the (exact) coalescent times along a sequence, as given by ms prime
+    # T_scaled_np are the scaled time intervals (in a np aray)
+    # N_T is the number of hidden states
+    coal_times_intervals = [] 
+    for i in range(0,len(coal_times)):
+        diff = coal_times[i] - T_scaled_np
+        diff_pos = diff[diff >0]
+        where = np.argmin(diff_pos) # find closest interval
+        if where == N_T: #TODO find a better fix for this. Have extended T to include upper interval
+            where = where - 1
+        coal_times_intervals.append(where) 
+    return coal_times_intervals
+
+# function to count the number of transitions between different states in a sequences of tMRCAs 
+def tm_counts(coal_times_intervals,N_T):
+    tm = np.zeros(shape=(N_T,N_T))
+    # tally the number of transitions from one interval to another (distributing over the rows)
+    for i in range(0,len(coal_times_intervals)-1):
+        col = coal_times_intervals[i+1]
+        row  = coal_times_intervals[i]
+        tm[row,col] += 1
+    return tm
+
+# function to return an array of coalescent data (tree index, intervals separated by recombination, tMRCA time) from an msprime simulation.
+def get_coal_data(sim,args):
+    tmrca_data = [] # first tuple is index, second tuple is sequence interval, third tuple is tMRCA
+    ind = 0 #index for binning
+    for tree in sim.trees():
+        if args.tree: #draw tree with info if that is required
+            print("tree {}: interval = {}".format(tree.index, tree.interval))
+            print("TMRCA: ",tree.time(tree.parent(0)))
+            print(tree.draw(format="unicode"))
+        tmrca_data.append((tree.index,tree.interval[1],tree.time(tree.parent(0))))
+        ind = math.floor(tree.interval[1]/args.bin_length)
+    return tmrca_data
+
+# function to divide the true coalescent data into bins of defined size
+def round_bin_coal_data(sim,tmrca_data,args):
+    coal_times = np.zeros(shape=(int(sim.sequence_length/args.bin_length))) # create empty array which will store the binned coalescent times
+    ind = 0 #index for binning
+    for i in range(0,len(tmrca_data)):
+        coal_times[ind:math.floor(tmrca_data[i][1]/args.bin_length)] = tmrca_data[i][2]
+        ind = math.floor(tmrca_data[i][1]/args.bin_length)
+    return coal_times
+
+# function to normalise a matrix. Can be normalised over either rows or columns, using either the sum or max
+# use sum for a probability distribution; use max for a good visual interpretation
+def normalise(matrix,how = 'colsum'):
+    options = ['rowmax', 'rowsum', 'colmax','colsum']
+    if how in options:
+        # actions for either row or column
+        possible_actions = ['row','col']
+        actions = ['[i,]','[:,i]']
+        # calcs for either sum or max
+        calcs = ['max','sum']
+        normalised_tm = np.array(matrix)
+        action = actions[possible_actions.index(how[0:3])]
+        calc = calcs[calcs.index(how[3:6])]
+        # check square
+        if matrix.shape[0] != matrix.shape[1]:
+            print('Warning! Matrix is not square. Continuing.') 
+        for i in range(0,matrix.shape[1]):
+            if max(matrix[:,i]) != 0.0:
+                execute_me = 'normalised_tm' + action + ' = normalised_tm' + action + ' / ' + calc + '(normalised_tm' + action + ')'
+                # print(execute_me)
+                exec(execute_me)
+        matrix = np.array(normalised_tm)
+    return matrix
